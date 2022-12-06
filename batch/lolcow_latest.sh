a7df44ea2569a4f146e66d3a11fd3be55b1ecf33
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-06 11:46:09 -0700

    Removed files temporarily.

--- batch/lolcow_latest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash --norc --noprofile -posix
-set -x -e
-cd /Users/henry.winterbottom/trunk/UFS-RNR.HenryWinterbottom-NOAA/ush/batch
-/usr/local/bin/singularity run ./tests/lolcow_latest.sif
-# Created: 18:20:49 Wednesday November 23, 2022
--- batch/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash --norc --noprofile -posix
-cd ./
-/usr/local/bin/singularity run innov_stats.sif
-# Created: 17:04:04 Wednesday November 23, 2022
