94e4dffcf1ec0217f79000960c177241e6f6a07a
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-13 13:38:47 -0700

    Added function and error class.

--- utils/timestamp_interface.py
+++ utils/timestamp_interface.py
@@ -32,10 +32,26 @@ Description
 Globals
 -------
 
-    timestamp_global: str
+    GLOBAL: str
 
         Global timestamp format; this is the format from which all
         others should be defined/determined.
+
+Classes
+-------
+
+    TimestampInterfaceError(msg)
+
+        This is the base-class for all exceptions; it is a sub-class
+        of Error.
+
+Functions
+---------
+
+    check_frmt(datestr, in_frmttyp = GLOBAL, out_frmttyp = GLOBAL)
+
+        This function checks that the format for a provided timestamp
+        matches the expected format.
 
 Author(s)
 ---------
@@ -51,6 +67,11 @@ History
 
 # ----
 
+from tools import datetime_interface
+from utils.error_interface import Error
+
+# ----
+
 __author__ = "Henry R. Winterbottom"
 __maintainer__ = "Henry R. Winterbottom"
 __email__ = "henry.winterbottom@noaa.gov"
@@ -59,4 +80,89 @@ __email__ = "henry.winterbottom@noaa.gov"
 
 # Global timestamp format; this is the format from which all others
 # should be defined/determined.
-timestamp_global = "%Y%m%d%H%M%S"
+GLOBAL = "%Y%m%d%H%M%S"
+
+# ----
+
+
+class TimestampInterfaceError(Error):
+    """
+    Description
+    -----------
+
+    This is the base-class for all exceptions; it is a sub-class of
+    Error.
+
+    Parameters
+    ----------
+
+    msg: str
+
+        A Python string to accompany the raised exception.
+
+    """
+
+    def __init__(self, msg: str):
+        """
+        Description
+        -----------
+
+        Creates a new TimestampInterfaceError object.
+
+        """
+        super(TimestampInterfaceError, self).__init__(msg=msg)
+
+
+# ----
+
+def check_frmt(datestr: str, in_frmttyp: str = GLOBAL,
+               out_frmttyp: str = GLOBAL) -> None:
+    """
+    Description
+    -----------
+
+    This function checks that the format for a provided timestamp
+    matches the expected format.
+
+    Parameters
+    ----------
+
+    datestr: str
+
+        A Python string specifying the timestamp.
+
+    Keywords
+    --------
+
+    in_frmttyp: str, optional
+
+        A Python string specifying the assumed format for the
+        timestamp string parameter; this assumes the POSIX UNIX
+        convention.
+
+    out_frmttyp: str, optional
+
+        A Python string specifying the expected format for the
+        timestamp string parameters; this assumes the POSIX UNIX
+        convention.
+
+    Raises
+    ------
+
+    TimestampInterfaceError:
+
+        * raised if the provided timestamp string is not of the proper
+          format.
+
+    """
+
+    # Define the timestamp string against which to compare the
+    # parameter specified upon entry.
+    check = datetime_interface.datestrupdate(datestr=datestr
+                                             in_frmttyp=in_frmttyp,
+                                             out_frmttyp=out_frmttyp)
+
+    if (check != datestr):
+        msg = (f'The timestamp string {datestr} does not match the format '
+               f'{out_frmttyp}. Aborting!!!')
+        raise TimestampInterfaceError(msg=msg)
