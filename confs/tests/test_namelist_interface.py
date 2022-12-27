711a00f4277534b5fdaf3d08929f29dcf988c6a1
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-27 08:55:18 -0700

    Updates via black.

--- confs/tests/test_namelist_interface.py
+++ confs/tests/test_namelist_interface.py
@@ -109,8 +109,7 @@ class TestNamelistMethods(TestCase):
 
         # Define the file paths required for the test method(s).
         dirpath = os.path.join(os.getcwd(), "tests")
-        self.nml_template = os.path.join(
-            dirpath, "test_files", "namelist.template")
+        self.nml_template = os.path.join(dirpath, "test_files", "namelist.template")
         self.nml_check = os.path.join(dirpath, "test_files", "namelist.check")
         self.nml_path = os.path.join(dirpath, "namelist.test")
 
