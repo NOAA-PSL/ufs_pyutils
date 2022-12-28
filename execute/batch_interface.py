7f400cd8228322de2117380fb0111a0b0689cb87
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2022-12-28 10:13:29 -0700

    Created branch for batch system support.

---
+++ execute/batch_interface.py
@@ -0,0 +1,49 @@
+# =========================================================================
+
+# Module: execute/batch_interface.py
+
+# Author: Henry R. Winterbottom
+
+# Email: henry.winterbottom@noaa.gov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the respective public license published by the
+# Free Software Foundation and included with the repository within
+# which this application is contained.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# =========================================================================
+
+"""
+
+
+"""
+
+# ----
+
+import re
+import subprocess
+
+from tools import parser_interface
+from utils.error_interface import Error
+from utils.logger_interfae import Logger
+
+# ----
+
+
+class Batch():
+    """
+
+    """
+
+    def __init__(self):
+        """
+        Description
+        -----------
+
+        Creates a new Batch object.
+ 
+        """
